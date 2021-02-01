bff5997e203234c162491c36cde67061a184bf4c
eddieDmv <47372079+eddieDmv@users.noreply.github.com>
2021-02-01 17:34:49 -0500

    Initial commit
    Initial commit

---
+++ Deck.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace deck_of_playing_cards
+{
+    class Deck
+    {
+        string[] stringCards = new string[]
+        {"Ace", "2", "3", "4", "5", "6", "7","8", "9", "10", "Jack", "Queen", "King"};
+
+        string[] suits = new string[]
+        {
+            "Clubs",
+            "Spades",
+            "Hearts",
+            "Diamonds"
+        };
+        public List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            foreach (string suit in suits)
+            {
+                int numVal = 1;
+
+                foreach (var stringVal in stringCards)
+                {
+                    Card newCard = new Card(stringVal, suit);
+                    cards.Add(newCard);
+                    numVal++;
+                }
+            }
+        }
+        public void ReadDeck()
+        {
+            System.Console.WriteLine("--- Reading Deck ---");
+            foreach (Card card in cards)
+            {
+                System.Console.WriteLine($"{card.stringVal} of {card.suit}");
+            }
+        }
+
+        public void Shuffle()
+        {
+            System.Console.WriteLine("---Shuffling---");
+            Random rand = new Random();
+            for (int i = 0; i < cards.Count; i++)
+            {
+                int newIndx = rand.Next(cards.Count);
+                Card temp = cards[i];
+                cards[i] = cards[newIndx];
+                cards[newIndx] = temp;
+            }
+        }
+        public Card DealOneCard()
+        {
+            Card dealtCard = cards[0];
+            cards.Remove(dealtCard);
+            return dealtCard;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace deck_of_playing_cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Player player1 = new Player("Eddie");
+            
+            Console.WriteLine("--- Deck before shuffling ---");
+            deck.ReadDeck();
+            
+            deck.Shuffle();
+            Console.WriteLine("--- Deck After shuffling ---");
+            deck.ReadDeck();
+
+            Console.WriteLine($"Card left: {deck.cards.Count}");
+            while (deck.cards.Count != 0)
+            {
+                player1.DealerDrawToCaller(deck, player1);                
+            }
+
+            Console.WriteLine($"Card left: {deck.cards.Count}");
+            player1.DealerDrawToCaller(deck, player1);
+            
+            player1.ReadPlayerHand(player1.hand);            
+        }
+    }
+}
